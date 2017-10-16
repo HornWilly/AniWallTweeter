81210d3fb8c46f42a1fff958ecfdb0ecaf76488f
Mehdi MAACHE <mehdi.maache@gmail.com>
2017-10-16 18:34:21 +0200

    Randomize and create a json anime list

--- app.php
+++ app.php
@@ -6,10 +6,17 @@ use \Aniwall\Service\WallhavenService;
 
 $wallhaven = new WallhavenService();
 
-$res = $wallhaven->search("attack on titans");
+$string    = file_get_contents("config/list_anime.json");
+$listAnime = json_decode($string, true);
+$nb        = rand(0, count($listAnime) - 1);
+$res       = $wallhaven->search($listAnime[$nb]);
 
-echo $res->getStatusCode();
-// "200"
-echo $res->getHeader('content-type');
-// 'application/json; charset=utf8'
-echo $res->getBody();
+echo 'Name: '.$listAnime[$nb].PHP_EOL;
+
+$listWallpaper = json_decode($res->getBody());
+$randWallpaper = rand(0, count($listWallpaper->images)-1);
+
+echo $listWallpaper->images[$randWallpaper]->thumb.PHP_EOL;
+
+$res = $wallhaven->get($listWallpaper->images[$randWallpaper]->id);
+echo $res->getBody().PHP_EOL;
